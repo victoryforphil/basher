68044cb081c84d7ebccb8fbbf8729949ba868654
Alex Carter <victoryforphil@gmail.com>
2024-09-29 23:56:27 -0700

    Added base systen module

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,4 @@
 mod basher_rerun;
+pub mod quad;
+pub mod state;
+pub mod system;
---
+++ src/system/runner.rs
@@ -0,0 +1,58 @@
+use std::time::{Duration, Instant};
+
+use crate::state::BasherState;
+
+use super::SystemHandle;
+
+pub struct BasherSysRunner {
+    pub systems: Vec<SystemHandle>,
+    pub state: BasherState,
+    end_time: Instant,
+}
+
+impl BasherSysRunner {
+    pub fn new() -> BasherSysRunner {
+        BasherSysRunner {
+            systems: Vec::new(),
+            state: BasherState {},
+            end_time: Instant::now(),
+        }
+    }
+
+    pub fn add_system(&mut self, system: SystemHandle) {
+        self.systems.push(system);
+    }
+
+    pub fn run(&mut self, duration: Duration) {
+        self.end_time = Instant::now() + duration;
+        for system in self.systems.iter_mut() {
+            system.init(&mut self.state);
+        }
+
+        while Instant::now() < self.end_time {
+            for system in self.systems.iter_mut() {
+                system.execute(&mut self.state);
+            }
+        }
+
+        for system in self.systems.iter_mut() {
+            system.cleanup(&mut self.state);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::system::mock::MockSystem;
+
+    #[test]
+    fn test_basher_sys_runner() {
+        let mut runner = BasherSysRunner::new();
+        let system_a = Box::new(MockSystem::new());
+        let system_b = Box::new(MockSystem::new());
+        runner.add_system(system_a);
+        runner.add_system(system_b);
+        runner.run(Duration::from_secs(1));
+    }
+}
